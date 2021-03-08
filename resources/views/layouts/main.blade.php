3a896a212ebf533d1ecf3f6b909add210fafe27a
Lucas <lucasdasilvabarbosa27@gmail.com>
2021-03-08 12:23:10 -0300

    Implementação do metodo de autenticação.
    Foi implementado o sistema de autenticação na aplicação.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -19,23 +19,46 @@
     <body>
       <header>
         <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="collapse navbar-collapse" id="navbar">
-            <a href="/" class="navbar-brand">
-              <img src="/img/hdcevents_logo.svg" alt="HDC Events">
+            <div class="collapse navbar-collapse" id="navbar">
+            <a href="/" name="logo" class="navbar-brand">
+                <img name="logo" id="logo" src="/img/logo.png" alt="HDC Events">
             </a>
-            <ul class="navbar-nav">
-              <li class="nav-item">
+            <ul name="menu" id="menu" class="navbar-nav">
+            <li class="nav-item">
                 <a href="/" class="nav-link">Eventos</a>
-              </li>
+            </li>
               <li class="nav-item">
                 <a href="/events/create" class="nav-link">Criar Eventos</a>
               </li>
-              <li class="nav-item">
-                <a href="/" class="nav-link">Entrar</a>
-              </li>
-              <li class="nav-item">
-                <a href="/" class="nav-link">Cadastrar</a>
-              </li>
+
+            @auth
+                <li class="nav-item">
+                    <a href="/dashboard" class="nav-link"> Meus Eventos</a>
+                </li>
+
+                <li class="nav-item">
+                    <form action="/logout" method="post">
+                    @csrf
+                        <a href="/logout"
+                            class="nav-link"
+                            onclick="event.preventDefault();
+                                    this.closest('form').submit();"
+                        >
+                        Sair
+                        </a>
+                    </form>
+                </li>
+            @endauth
+
+
+            @guest
+                <li class="nav-item">
+                    <a href="/login" class="nav-link">Entrar</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/register" class="nav-link">Cadastrar</a>
+                </li>
+            @endguest
             </ul>
           </div>
         </nav>
